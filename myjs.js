7fa6dc977ddf559b6d808a834fc4430f196e7624
Manav89 <mmpatel151@gmail.com>
2023-09-05 09:20:49 +0530

    first commit

---
+++ myjs.js
@@ -0,0 +1,46 @@
+function red()
+{
+    document.getElementById("ww").style.backgroundColor="#e60000";
+    document.getElementById("w").style.backgroundColor="red";
+    document.getElementById("w").style.backgroundColor="red";
+}
+function orange()
+{
+    document.getElementById("ww").style.backgroundColor="#e69500";
+    document.getElementById("w").style.backgroundColor="orange";
+    document.getElementById("w").style.backgroundColor="orange";
+}
+
+function yellow()
+{
+    document.getElementById("ww").style.backgroundColor="#ffff00";
+    document.getElementById("w").style.backgroundColor="#ffff66";
+    document.getElementById("w").style.backgroundColor="#ffff66";
+}
+
+function green()
+{
+    document.getElementById("ww").style.backgroundColor="#00e600";
+    document.getElementById("w").style.backgroundColor="green";
+    document.getElementById("w").style.backgroundColor="green";
+}
+
+function blue()
+{
+    document.getElementById("ww").style.backgroundColor="#0000e6";
+    document.getElementById("w").style.backgroundColor="blue";
+    document.getElementById("w").style.backgroundColor="blue";
+}
+function indigo()
+{
+    document.getElementById("ww").style.backgroundColor="#590099";
+    document.getElementById("w").style.backgroundColor="indigo";
+    document.getElementById("w").style.backgroundColor="indigo";
+}
+
+function violet()
+{
+    document.getElementById("ww").style.backgroundColor="#ed78ed";
+    document.getElementById("w").style.backgroundColor="violet";
+    document.getElementById("w").style.backgroundColor="violet";
+}
